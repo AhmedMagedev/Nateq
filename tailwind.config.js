8db161271a20d1c009055ec5917ec0b5cd0ccdbd
Ahmed Maged <ahmedmaged20188@gmail.com>
2023-08-20 17:40:55 +0300

    Coming soon stage

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    colors: {
+      brandCream: "#FFFEF4",
+      brandYellow: "#FDE1A3",
+      brandBrown: "#FDE1A3",
+      brandred: "#EB5066",
+    },
+    extend: {
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden",
+          },
+          "100%": {
+            width: "100%",
+          },
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent",
+          },
+          "100%": {
+            borderColor: "white",
+          },
+        },
+      },
+      animation: {
+        typing: "typing 5s steps(20) infinite alternate, blink .7s infinite",
+      },
+    },
+  },
+  plugins: [],
+};
