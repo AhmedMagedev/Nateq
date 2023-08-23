be8a44def1a201ba92c77852ec022fd127a99efe
Ahmed Maged <ahmedmaged20188@gmail.com>
2023-08-23 19:35:54 +0300

    favicon

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import "tailwindcss/tailwind.css";
+import Head from "next/head";
 import Image from "next/image";
 import logo from "../public/logo.svg";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-brandCream p-10">
+      <Head>
+        <title>Nateq</title>
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+
       <div className="w-max">
         <Image src={logo} height={200} width={200} className=" m-auto" />
         <h1 className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-5xl text-brandred font-bold py-4">
